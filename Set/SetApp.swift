16870a92e0cf06766704b36c1381dd549df212c4
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2022-12-12 15:47:14 +0530

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Sanjay Siddharth on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
